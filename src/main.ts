8fe99c03f6fd56d2aa9455151a4b26826e5ffa9d
unknown <waleed.ali555556@gmail.com>
2022-12-09 18:10:43 +0500

    secondCommit

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import { Amplify } from 'aws-amplify';
+const awsConfig=require('./aws-exports')
 
+Amplify.configure(awsConfig) 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
